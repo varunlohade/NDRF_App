f8ec14a8100002df4d445e7b02d1164555757f11
varunlohade <varunlohade@gmail.com>
2022-08-26 08:41:32 +0530

    added icons

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:map_launcher/map_launcher.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,6 +11,40 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  openMapsSheet(context) async {
+    try {
+      final coords = Coords(17.6951101327645, 75.28089164849547);
+      final title = "Flood Alert";
+      final availableMaps = await MapLauncher.installedMaps;
+
+      showModalBottomSheet(
+        context: context,
+        builder: (BuildContext context) {
+          return SafeArea(
+            child: SingleChildScrollView(
+              child: Container(
+                child: Wrap(
+                  children: <Widget>[
+                    for (var map in availableMaps)
+                      ListTile(
+                        onTap: () => map.showMarker(
+                          coords: coords,
+                          title: title,
+                        ),
+                        title: Text(map.mapName),
+                      ),
+                  ],
+                ),
+              ),
+            ),
+          );
+        },
+      );
+    } catch (e) {
+      print(e);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,7 +57,7 @@ class _HomePageState extends State<HomePage> {
               Container(
                 child: Text(
                   "NDRF",
-                  style: TextStyle(fontSize: 30),
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
                 ),
               ),
               SizedBox(
@@ -52,74 +87,172 @@ class _HomePageState extends State<HomePage> {
                           itemBuilder: (BuildContext context, int index) {
                             DocumentSnapshot course =
                                 snapshot.data!.docs[index];
-                            return Column(
-                              children: [
-                                Container(
-                                  height: 200,
-                                  width: 300,
-                                  decoration: BoxDecoration(
-                                      color: Color.fromARGB(255, 219, 219, 219),
-                                      borderRadius: BorderRadius.circular(20)),
-                                  child: Padding(
-                                    padding: const EdgeInsets.all(10.0),
-                                    child: Column(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.start,
-                                      children: [
-                                        Text("Location: Solapur"),
-                                        SizedBox(
-                                          height: 10,
+                            return Center(
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  Center(
+                                    child: Container(
+                                      // height: 200,
+                                      width: 300,
+                                      decoration: BoxDecoration(
+                                          color: Color.fromARGB(
+                                              255, 219, 219, 219),
+                                          borderRadius:
+                                              BorderRadius.circular(20)),
+                                      child: Padding(
+                                        padding: const EdgeInsets.all(10.0),
+                                        child: Column(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.start,
+                                          children: [
+                                            Text.rich(TextSpan(
+                                                text: "Location: ",
+                                                children: [
+                                                  TextSpan(
+                                                      text: "Pandharpur ",
+                                                      style: TextStyle(
+                                                          fontSize: 15,
+                                                          fontWeight:
+                                                              FontWeight.w600)),
+                                                ])),
+                                            SizedBox(
+                                              height: 10,
+                                            ),
+                                            Text.rich(TextSpan(
+                                                text: "Severity: ",
+                                                children: [
+                                                  TextSpan(
+                                                      text:
+                                                          "${course['severity']} ",
+                                                      style: TextStyle(
+                                                          fontSize: 15,
+                                                          fontWeight:
+                                                              FontWeight.w600,
+                                                          color:
+                                                              course['severity'] ==
+                                                                      'Extreme'
+                                                                  ? Colors.red
+                                                                  : Colors
+                                                                      .orange)),
+                                                ])),
+                                            // Text("Severity: ${course['severity']}"),
+                                            SizedBox(
+                                              height: 10,
+                                            ),
+                                            Text.rich(TextSpan(
+                                                text: "RWL: ",
+                                                children: [
+                                                  TextSpan(
+                                                      text:
+                                                          "${course['RWL']} m ",
+                                                      style: TextStyle(
+                                                          fontSize: 15,
+                                                          fontWeight:
+                                                              FontWeight.w600)),
+                                                ])),
+                                            // Text("RWL: ${course['RWL']}"),
+                                            SizedBox(
+                                              height: 10,
+                                            ),
+                                            Text.rich(TextSpan(
+                                                text:
+                                                    "RWL at flood discharge:: ",
+                                                children: [
+                                                  TextSpan(
+                                                      text:
+                                                          "${course['RWL at flood discharge']} cusecs",
+                                                      style: TextStyle(
+                                                          fontSize: 15,
+                                                          fontWeight:
+                                                              FontWeight.w600)),
+                                                ])),
+                                            // Text(
+                                            //     "RWL at flood discharge: ${course['RWL at flood discharge']}"),
+                                            SizedBox(
+                                              height: 10,
+                                            ),
+                                            Text.rich(TextSpan(
+                                                text: "Velocity: ",
+                                                children: [
+                                                  TextSpan(
+                                                      text:
+                                                          "${course['velocity']} m/sec",
+                                                      style: TextStyle(
+                                                          fontSize: 15,
+                                                          fontWeight:
+                                                              FontWeight.w600)),
+                                                ])),
+                                            // Text("Velocity: ${course['velocity']}"),
+                                            SizedBox(
+                                              height: 10,
+                                            ),
+                                            Row(
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment.center,
+                                              children: [
+                                                Text.rich(
+                                                  TextSpan(
+                                                      text: "Depth: ",
+                                                      children: [
+                                                        TextSpan(
+                                                            text:
+                                                                "${course['depth']} m",
+                                                            style: TextStyle(
+                                                                fontSize: 15,
+                                                                fontWeight:
+                                                                    FontWeight
+                                                                        .w600)),
+                                                      ]),
+                                                ),
+                                                SizedBox(width: 10),
+                                                course['velocity'] *
+                                                            course['depth'] >
+                                                        7.44
+                                                    ? Icon(
+                                                        Icons.car_crash,
+                                                        color: Colors.redAccent,
+                                                      )
+                                                    : Icon(
+                                                        Icons.flood,
+                                                        color:
+                                                            Colors.blueAccent,
+                                                      )
+                                              ],
+                                            ),
+                                            SizedBox(
+                                              height: 10,
+                                            ),
+                                            InkWell(
+                                              onTap: () async {
+                                                openMapsSheet(context);
+                                              },
+                                              child: Container(
+                                                height: 50,
+                                                width: 90,
+                                                decoration: BoxDecoration(
+                                                    borderRadius:
+                                                        BorderRadius.circular(
+                                                            20),
+                                                    color: Colors.red),
+                                                child: Center(
+                                                    child: Text(
+                                                  "Locate",
+                                                  style: TextStyle(
+                                                      color: Colors.white),
+                                                )),
+                                              ),
+                                            )
+                                          ],
                                         ),
-                                        Text("Severity: ${course['severity']}"),
-                                        SizedBox(
-                                          height: 10,
-                                        ),
-                                        Text("RWL: ${course['RWL']}"),
-                                        SizedBox(
-                                          height: 10,
-                                        ),
-                                        Text(
-                                            "RWL at flood discharge: ${course['RWL at flood discharge']}"),
-                                        SizedBox(
-                                          height: 10,
-                                        ),
-                                        Text("Velocity: ${course['velocity']}"),
-                                        SizedBox(
-                                          height: 10,
-                                        ),
-                                        InkWell(
-                                          onTap: () async {
-                                            String googleUrl =
-                                                'https://www.google.com/maps/search/?api=1&query=${75.28089164849547},${17.6951101327645}';
-                                            if (await canLaunch(googleUrl)) {
-                                              await launch(googleUrl);
-                                            } else {
-                                              throw 'Could not open the map.';
-                                            }
-                                          },
-                                          child: Container(
-                                            height: 50,
-                                            width: 90,
-                                            decoration: BoxDecoration(
-                                                borderRadius:
-                                                    BorderRadius.circular(20),
-                                                color: Colors.red),
-                                            child: Center(
-                                                child: Text(
-                                              "Locate",
-                                              style: TextStyle(
-                                                  color: Colors.white),
-                                            )),
-                                          ),
-                                        )
-                                      ],
+                                      ),
                                     ),
                                   ),
-                                ),
-                                SizedBox(
-                                  height: 10,
-                                )
-                              ],
+                                  SizedBox(
+                                    height: 10,
+                                  )
+                                ],
+                              ),
                             );
                           },
                         );
@@ -138,12 +271,12 @@ class _HomePageState extends State<HomePage> {
               //       children: [
               //         Column(
               //           children: const [
-              //             Text.rich(TextSpan(text: "Location: ", children: [
-              //               TextSpan(
-              //                   text: "Pandharpur ",
-              //                   style: TextStyle(
-              //                       fontSize: 15, fontWeight: FontWeight.w600)),
-              //             ])),
+              // Text.rich(TextSpan(text: "Location: ", children: [
+              //   TextSpan(
+              //       text: "Pandharpur ",
+              //       style: TextStyle(
+              //           fontSize: 15, fontWeight: FontWeight.w600)),
+              // ])),
               //             // Text("Location: Pandharpur"),
               //             SizedBox(
               //               height: 10,
